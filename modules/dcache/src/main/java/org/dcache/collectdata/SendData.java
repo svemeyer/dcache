7636b8aa231ec1a88e8c136fd974344281403c35
Svenja Meyer <17570879+xpinkyx@users.noreply.github.com>
2020-01-30 10:41:34 +0100

    Make sending data work without interaction via admin interface

--- modules/dcache/src/main/java/org/dcache/collectdata/SendData.java
+++ modules/dcache/src/main/java/org/dcache/collectdata/SendData.java
@@ -1,66 +1,95 @@
 package org.dcache.collectdata;
 
 import dmg.cells.nucleus.CellCommandListener;
-import dmg.util.command.Command;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.Callable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
 
 public class SendData implements CellCommandListener {
-    private Timer timer;
-    private InstanceData instanceData;
     private static final Logger LOGGER = LoggerFactory.getLogger(SendData.class);
+    private ScheduledExecutorService sendDataExecutor;
+    private InstanceData instanceData;
 
     @Required
     public void setInstanceData(InstanceData instanceData) {
         this.instanceData = instanceData;
     }
 
-    private int sendData() throws java.io.IOException {
+    public SendData() {
+        sendDataExecutor = Executors.newScheduledThreadPool(1);
+    }
+
+    public void init() {
+        LOGGER.warn("Sending information about dCache-instance will be activated");
+        ScheduledFuture sendDataExecFuture = sendDataExecutor.scheduleAtFixedRate(this::sendData,
+                0, 1, TimeUnit.HOURS);
+    }
+
+    public void shutdown() {
+        sendDataExecutor.shutdown();
+    }
+
+    private int sendData() {
         instanceData.refreshData();
+
         String json_body = instanceData.toJson();
-        URL url = new URL("https://xxxxxxxx/collector");
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+        URL url;
+        HttpsURLConnection conn;
+        int response_code = 0;
+        try {
+            url = new URL("https://xxxxxxxx/collector?date=" + format.format(new Date()));
+        } catch (MalformedURLException mue) {
+            LOGGER.error("URL is in wrong format.");
+            return 0;
+        }
 
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try {
+            conn = (HttpsURLConnection) url.openConnection();
+        } catch (java.io.IOException ioe) {
+            LOGGER.error("Connection failed");
+            return 0;
+        }
 
-        conn.setRequestMethod("PUT");
+        try {
+            conn.setRequestMethod("POST");
+        } catch (ProtocolException pe) {
+            LOGGER.error("Error with Protocol");
+            return 0;
+        }
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setDoOutput(true);
 
-        OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
-        out.write(json_body);
-        out.close();
-
-        return conn.getResponseCode();
-    }
-
-    @Command(name = "send", hint = "Start sending data", description = "Start collecting instance information and " +
-            "sending them to collector")
-    public class StartSend implements Callable<String> {
-        public String call() {
-            try {
-                timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        try {
-                            sendData();
-                        } catch (Exception e) {
-                            timer.cancel();
-                        }
-                    }
-                }, 60000);
-            } catch (Exception e) {
-                return e.toString();
+        try {
+            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+            out.write(json_body);
+            out.close();
+            response_code = conn.getResponseCode();
+            if (response_code != 200 && response_code != 201) {
+                LOGGER.error("Error sending the data. Response: " + response_code + " " + conn.getResponseMessage());
+            } else {
+                LOGGER.warn("Information sent to collector.");
             }
-            return "-";
+        } catch (java.io.IOException ioe) {
+            LOGGER.error("Error while sending to collector: " + ioe);
+            return 0;
         }
+
+        try {
+            response_code = conn.getResponseCode();
+        } catch (java.io.IOException ioe) {
+            LOGGER.error("Error getting response code: " + ioe);
+            return 0;
+        }
+        return response_code;
     }
 }
